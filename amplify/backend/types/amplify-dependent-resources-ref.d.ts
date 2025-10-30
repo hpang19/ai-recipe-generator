68e1164da3e2a1f4d8b95433e5b93f76dc870e05
Hsin Pang <hpang19@my.bcit.ca>
2025-10-29 22:36:48 -0700

    updates

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "airecipegenerator9a7fd2a5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/aws-exports.d.ts
@@ -0,0 +1,5 @@
+declare module "./aws-exports" {
+    const awsExports: any;
+    export default awsExports;
+  }
+  
